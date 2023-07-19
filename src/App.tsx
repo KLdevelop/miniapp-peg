573367ccfffbc674226485e4a7cef7dba8485b01
KLdevelop <drapdrop1@gmail.com>
2023-07-19 21:08:29 +0300

    setted global app styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import styles from 'app.module.scss';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'app.module.scss';
 
 export const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<div className={styles.app}>App</div>} />
+      <Route path="/" element={<div>App</div>} />
     </Routes>
   </Router>
 );
