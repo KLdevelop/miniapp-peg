bff9ed30694ffb5c635b9ebe9a137d54c1d12142
KLdevelop <drapdrop1@gmail.com>
2023-10-01 11:23:20 +0300

    no need for redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import { App } from './App';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './models/store';
 
 const AppElement = document.getElementById('app') || document.createElement('div');
 AppElement.id = 'app';
 const root = createRoot(AppElement);
 
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-);
+root.render(<App />);
